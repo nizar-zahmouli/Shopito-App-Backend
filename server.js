fc0da660093e67d4b59ffacdbce3886f05d7af38
Nizar <you@example.com>
2025-01-02 17:28:53 +0100

    backend

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-   cors({ origin: ["http://localhost:3000", ""],
+   cors({  origin: ["http://localhost:3000", "https://shopitoApp.vercel.app"],
     credentials: true})
 )
 // Routes 
